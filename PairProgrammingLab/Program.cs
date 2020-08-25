2bd74836c2035e90d50a233039cfac3f1c3ca3a2
jonmboyce <jonmboyce@outlook.com>
2020-08-25 15:20:53 -0500

    Add project files.

---
+++ PairProgrammingLab/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgrammingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> students = new List<string>();
+            bool nameEntered = false;
+
+
+            Console.WriteLine("Enter new student names.");
+            string newStudent = Console.ReadLine();
+            do
+            {
+
+            
+            if (newStudent.Length<1)
+            {
+                Console.WriteLine("Sorry, no characters receiveed.");
+            }
+            else
+            {
+                Console.WriteLine(newStudent + "***");
+                students.Add(newStudent);
+            }
+            } while (nameEntered);
+        }
+    }
+}
