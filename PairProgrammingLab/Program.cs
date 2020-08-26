d67309430d52f02d097fc1f029fee5b5b22ede04
jonmboyce <jonmboyce@outlook.com>
2020-08-26 09:48:56 -0500

    more work on the students lab

--- PairProgrammingLab/Program.cs
+++ PairProgrammingLab/Program.cs
@@ -12,24 +12,45 @@ namespace PairProgrammingLab
         {
             List<string> students = new List<string>();
             bool nameEntered = false;
+            string userNameEntered = "";
 
-
-            Console.WriteLine("Enter new student names.");
-            string newStudent = Console.ReadLine();
             do
             {
+                Console.WriteLine("Please enter the student's name");
+                userNameEntered = Console.ReadLine();
 
-            
-            if (newStudent.Length<1)
-            {
-                Console.WriteLine("Sorry, no characters receiveed.");
+                if (userNameEntered.Count() > 0)
+                {
+                    students.Add(userNameEntered);
+                }
+                else
+                {
+                    nameEntered = true;
+                }
+
             }
-            else
+            while (!nameEntered);
+            do
             {
-                Console.WriteLine(newStudent + "***");
-                students.Add(newStudent);
-            }
-            } while (nameEntered);
+                if (students.Count ==3)
+                {
+                    Console.WriteLine($"Student group:\n{students[0]}\n{students[1]}\n{students[2]}");
+                    students.RemoveRange(0, 3);
+                }
+                else if (students.Count > 3)
+                {
+                    Console.WriteLine($"Student group:\n{students[0]}\n{students[1]}\n");
+                    students.RemoveRange(0,2);
+                }
+                else
+                {
+                    Console.WriteLine($"Student group:\n{students[0]}\n{students[1]}\n");
+                    students.RemoveRange(0, 2);
+                }
+
+            } while (students.Count > 0);
+
         }
     }
 }
+
